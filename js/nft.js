d03525bfc1ab9b8918e7f035de46896f23c4630f
zamao9 <94758143+zamao9@users.noreply.github.com>
2022-02-19 00:32:41 +0300

    safsaf

--- js/nft.js
+++ js/nft.js
@@ -62,6 +62,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
+  /* АНИМАЦИЯ ПРИ СКРОЛЛЕ */
+
+  AOS.init({
+    disable: false,
+    startEvent: 'DOMContentLoaded',
+    initClassName: 'aos-init',
+    animatedClassName: 'aos-animate',
+    useClassNames: false,
+    disableMutationObserver: false,
+    debounceDelay: 50,
+    throttleDelay: 99,
+
+    offset: 10,
+    delay: 0,
+    duration: 1000,
+    easing: 'ease',
+    once: true,
+    mirror: false,
+    anchorPlacement: 'top-top',
+  })
+
+
+
+
+
 
 
 
